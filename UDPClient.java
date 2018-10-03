811922b180b73424976964be11e3ec014997d7b5
Kevin Phelan <tromboneist16@gmail.com>
2018-10-02 23:06:43 -0400

    updating documentation

--- UDPClient.java
+++ UDPClient.java
@@ -17,16 +17,16 @@ import java.util.Scanner;
 public class UDPClient {
     public static final int SERVER_PORT=5432;
     public static void main(String[] args)throws IOException{
-        DatagramSocket ds = new DatagramSocket();
+        DatagramSocket ds = new DatagramSocket();//making the socket
         
-        Scanner in = new Scanner(System.in);
-        String userInput;// get rid of the in.next for the while
+        Scanner in = new Scanner(System.in);//make all the variables
+        String userInput;
         byte[] b;
         
         while ((userInput = in.next()) != null) {
         b = (userInput).getBytes();
         
-        InetAddress ia = InetAddress.getLocalHost();
+        InetAddress ia = InetAddress.getLocalHost();//geting the IP to make sure its safe
         DatagramPacket dp = new DatagramPacket(b,b.length,ia,SERVER_PORT);
         ds.send(dp);
         }
